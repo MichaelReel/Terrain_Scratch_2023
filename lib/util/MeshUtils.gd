abc95bd5934e00f5dac36ceb2789601978cf8558
Michael Reel <mykilr@gmail.com>
2023-01-30 23:39:09 +0000

    Set a material in water bodies and added the surrounding sea as neat fitting plane

--- lib/util/MeshUtils.gd
+++ lib/util/MeshUtils.gd
@@ -21,25 +21,43 @@ static func get_land_mesh(high_level_terrain: HighlevelTerrain, debug_color_dict
 	
 	return island_mesh
 
-static func get_water_body_meshes(high_level_terrain: HighlevelTerrain, debug_color_dict: DebugColorDict) -> Array:  # -> Array(Mesh)
+static func get_water_body_meshes(high_level_terrain: HighlevelTerrain) -> Array:  # -> Array[Mesh]
 	var meshes: Array = []
 	for lake in high_level_terrain.get_lakes():
-		meshes.append(_get_water_body_mesh(lake, debug_color_dict))
+		meshes.append(_get_water_body_mesh(lake))
+	meshes.append(_get_sea_level_mesh(high_level_terrain.grid))
 	return meshes
 
-static func _get_water_body_mesh(lake: Region, debug_color_dict: DebugColorDict) -> Mesh:
+static func _get_water_body_mesh(lake: Region) -> Mesh:
 	var surface_tool: SurfaceTool = SurfaceTool.new()
 	var lake_mesh: Mesh = Mesh.new()
 	
 	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
 	for triangle in lake.get_cells():
-		var color_dict: Dictionary = triangle.get_river_vertex_colors(
-			debug_color_dict.river_color, debug_color_dict.base_color, debug_color_dict.head_color, debug_color_dict.mouth_color
-		)
 		for vertex in triangle.get_vertices():
-			surface_tool.add_color(color_dict[vertex])
 			surface_tool.add_vertex(vertex.get_vector_at_height(lake.get_water_height()))
+	
 	surface_tool.generate_normals()
 	var _err = surface_tool.commit(lake_mesh)
 	
 	return lake_mesh
+
+static func _get_sea_level_mesh(grid: Grid) -> Mesh:
+	var surface_tool: SurfaceTool = SurfaceTool.new()
+	var sea_mesh: Mesh = Mesh.new()
+	
+	surface_tool.begin(Mesh.PRIMITIVE_TRIANGLES)
+	for row in grid.get_triangles():
+		for triangle in row:
+			if triangle.get_parent() != null:
+				continue
+			
+			for vertex in triangle.get_vertices():
+				surface_tool.add_vertex(vertex.get_vector_at_height(0.0))
+	
+	surface_tool.generate_normals()
+	var _err = surface_tool.commit(sea_mesh)
+	
+	return sea_mesh
+	
+
--- view/IslandMesh.gd
+++ view/IslandMesh.gd
@@ -47,15 +47,16 @@ func _on_stage_complete(stage: Stage) -> void:
 func _on_all_stages_complete() -> void:
 	var island_mesh: Mesh = MeshUtils.get_land_mesh(high_level_terrain, debug_color_dict)
 	set_mesh(island_mesh)
-	_create_water_mesh_instances()
+	_create_water_mesh_instances(load("res://materials/water_surface.tres"))
 	print("High Level Terrain stages complete")
 
-func _create_water_mesh_instances() -> void:
+func _create_water_mesh_instances(water_material: Material) -> void:
 	# TODO: Consider how this can be separated from the land mesh in a nice way
-	var meshes = MeshUtils.get_water_body_meshes(high_level_terrain, debug_color_dict)
+	var meshes: Array = MeshUtils.get_water_body_meshes(high_level_terrain)
 	for water_mesh in meshes:
 		var mesh_instance: MeshInstance = MeshInstance.new()
-		# TODO: fix the material properties so colors, etc are visible
 		mesh_instance.mesh = water_mesh
+		# TODO: fix the material properties so colors, etc are visible
+		mesh_instance.set_surface_material(0, water_material)
 		add_child(mesh_instance)
 
